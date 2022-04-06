1b3399b33cd70416901aea10ee48b91bdd85600b
JordanWeitz <77029445+JordanWeitz@users.noreply.github.com>
2022-04-06 11:05:17 -0600

    Added counter for individual items

--- src/App.js
+++ src/App.js
@@ -5,61 +5,61 @@ import Search from "./components/search/Search";
 import Selected from "./components/selected/Selected";
 import Counter from "./components/counter/Counter";
 import Title from "./components/title/Title";
-import "./app.css";
+import "./App.css";
 
 function App() {
-	const [results, setResults] = useState([]);
-	const [searchEmpty, setSearchEmpty] = useState(true);
-	const [selected, setSelected] = useState([]);
+  const [results, setResults] = useState([]);
+  const [searchEmpty, setSearchEmpty] = useState(true);
+  const [selected, setSelected] = useState([]);
 
-	const handleSearch = (e) => {
-		const query = e.target.value.toUpperCase();
-		if (query.length === 0) {
-			setSearchEmpty(true);
-		}
+  const handleSearch = (e) => {
+    const query = e.target.value.toUpperCase();
+    if (query.length === 0) {
+      setSearchEmpty(true);
+    }
 
-		if (query.length < 2) {
-			return;
-		}
+    if (query.length < 2) {
+      return;
+    }
 
-		axios.get("https://api.frontendeval.com/fake/food/" + query).then((res) => {
-			setResults(res.data);
-			setSearchEmpty(false);
-		});
-	};
+    axios.get("https://api.frontendeval.com/fake/food/" + query).then((res) => {
+      setResults(res.data);
+      setSearchEmpty(false);
+    });
+  };
 
-	const addItem = (item) => {
-		if (selected.includes(item)) {
-			alert("Item already selected");
-			return;
-		}
-		const temp = [...selected, item];
+  const addItem = (item) => {
+    if (selected.includes(item)) {
+      alert("Item already selected");
+      return;
+    }
+    const temp = [...selected, item];
 
-		setSelected(temp.sort());
+    setSelected(temp.sort());
 
-		console.log(selected);
-	};
+    console.log(selected);
+  };
 
-	const removeItem = (item) => {
-		const temp = selected.filter((i) => i !== item);
-		setSelected(temp.sort());
-	};
+  const removeItem = (item) => {
+    const temp = selected.filter((i) => i !== item);
+    setSelected(temp.sort());
+  };
 
-	return (
-		<div className="App">
-			<Title />
-			<div className="lower__wrapper">
-				<div className="grocery__wrapper">
-					<div className="container">
-						<Search handleSearch={handleSearch} searchEmpty={searchEmpty} />
-						{searchEmpty ? <></> : <List results={results} addItem={addItem} />}
-						<Selected selected={selected} removeItem={removeItem} />
-						<Counter value={selected.length} />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Title />
+      <div className="lower__wrapper">
+        <div className="grocery__wrapper">
+          <div className="container">
+            <Search handleSearch={handleSearch} searchEmpty={searchEmpty} />
+            {searchEmpty ? <></> : <List results={results} addItem={addItem} />}
+            <Selected selected={selected} removeItem={removeItem} />
+            <Counter value={selected.length} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/selectedItem/SelectedItem.js
+++ src/components/selectedItem/SelectedItem.js
@@ -2,18 +2,46 @@ import React from "react";
 
 import "./selectedItem.css";
 
-import { IconContext } from "react-icons";
 import { BsFillDashCircleFill } from "react-icons/bs";
 
 function SelectedItem({ item, removeItem }) {
-	return (
-		<div className="selected__item">
-			<div className="selected__item__X" onClick={() => removeItem(item)}>
-				<BsFillDashCircleFill />
-			</div>
-			<div className="selected__item__name">{item.toUpperCase()}</div>
-		</div>
-	);
+  const [count, setCount] = React.useState(1);
+
+  const handleCount = (e) => {
+    if (e.target.innerText === "+") {
+      setCount(count + 1);
+    } else if (e.target.innerText === "-") {
+      if (count < 2) {
+        removeItem(item);
+      } else {
+        setCount(count - 1);
+      }
+    }
+  };
+
+  return (
+    <div className="selected__item">
+      <div className="selected__item__X" onClick={() => removeItem(item)}>
+        <BsFillDashCircleFill />
+      </div>
+      <div className="selected__item__name">{item.toUpperCase()}</div>
+      <div className="selected__item__buttons__wrapper">
+        <button
+          className="selected__item__button"
+          onClick={(e) => handleCount(e)}
+        >
+          +
+        </button>
+        <button
+          className="selected__item__button"
+          onClick={(e) => handleCount(e)}
+        >
+          -
+        </button>
+      </div>
+      <span className="selected__item__count">{count}</span>
+    </div>
+  );
 }
 
 export default SelectedItem;
